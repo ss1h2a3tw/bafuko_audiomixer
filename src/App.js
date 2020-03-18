7b357b1459c184ac1874c28dacd9e81eb1f42c9d
Shane Chen <ss1ha3tw@gmail.com>
2020-03-18 23:27:19 +0800

    Add basic loading and GCS CORS config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TEST_DRUM = 'https://audio_bafuko_moe.storage.googleapis.com/drum.wav';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    };
+  }
+  componentDidMount() {
+    fetch(TEST_DRUM)
+      .then(res => res.blob())
+      .then(res => {
+        this.drum = res;
+        this.filesize = res.size;
+        this.setState({ loaded: true });
+      });
+  }
+  render() {
+    if (!this.state.loaded) {
+      return <>LOADING!</>;
+    } else {
+      return <>{this.filesize}</>;
+    }
+  }
 }
-
 export default App;
