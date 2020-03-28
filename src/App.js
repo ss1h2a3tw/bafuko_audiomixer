3ccbef5a107dc66209084b3424353c103b835e2b
Shane Chen <ss1ha3tw@gmail.com>
2020-03-29 00:04:34 +0800

    load metadata

--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
 import React from 'react';
 
-const TEST_DRUM = 'https://audio_bafuko_moe.storage.googleapis.com/drum.mp3';
+const META_URL = 'https://audio_bafuko_moe.storage.googleapis.com/meta.json';
+
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaded: false
+      metaLoaded: false,
     };
+    this.audio = [];
   }
   componentDidMount() {
     this.actx = new AudioContext();
-    fetch(TEST_DRUM)
-      .then(res => res.blob())
-      .then(res => {
-        this.drumblob = res;
-        this.drum = new Audio(URL.createObjectURL(res));
-        this.drumtrack = this.actx.createMediaElementSource(this.drum);
-        this.drumtrack.connect(this.actx.destination);
-        this.filesize = res.size;
-        this.setState({ loaded: true });
+    fetch(META_URL)
+      .then((res) => res.json())
+      .then((res) => {
+        window.console.log(res);
+        let nextState = {
+          metaLoaded: true,
+          audioLoaded: [],
+        };
+        let pendingToLoad = [];
+        this.audio = res;
+        for (const idx in res) {
+          nextState.audioLoaded.push(false);
+          if (res[idx].default) {
+            pendingToLoad.push(idx);
+          }
+        }
+        this.setState(nextState);
+        for (const idx of pendingToLoad) {
+          this.loadAudio(idx);
+        }
+      });
+  }
+  loadAudio(idx) {
+    fetch(this.audio[idx].url)
+      .then((res) => res.blob())
+      .then((res) => {
+        let audio = this.audio[idx];
+        audio.blob = res;
+        audio.audioNode = new Audio(URL.createObjectURL(res));
+        audio.audioSource = this.actx.createMediaElementSource(audio.audioNode);
+        audio.audioSource.connect(this.actx.destination);
+        let newState = {};
+        this.setState((prevState) => {
+          let nextAudioLoaded = [...prevState.audioLoaded];
+          nextAudioLoaded[idx] = true;
+          return { audioLoaded: nextAudioLoaded };
+        });
+        window.console.log(this.audio);
       });
   }
   play() {
     this.drum.play();
   }
   render() {
-    if (!this.state.loaded) {
+    if (!this.state.metaLoaded) {
       return <>LOADING!</>;
     } else {
       return (
